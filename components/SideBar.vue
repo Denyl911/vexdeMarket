3a26882c234573a65cd0f644c60e362d1a237787
e-duardo-lucas <el035627@gmail.com>
2024-03-01 19:55:12 -0600

    Segundo cambio

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,23 +1,38 @@
 <template>
-  <div class="bg-black text-white fixed top-0 left-0 right-0 shadow-md flex items-center h-16 justify-between">
-    <!-- Ícono apertura -->
-    <button class="bg-transparent ml-2" @click="openMenu" v-if="!showMenu">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />        
-      </svg>
-    </button>
-    
-    <!-- Input para búsqueda -->
-    <input type="text" class="bg-transparent text-white border-b border-blue-500 rounded-xl mx-2 w-56 sm:w-96 focus:border-red-500 outline-none" placeholder="   Buscar en GOING" />
+  <div>
+    <!-- Barra superior -->
+    <div class="bg-black text-white fixed top-0 left-0 right-0 shadow-md flex items-center h-16 justify-between">
+      <!-- Ícono apertura -->
+      <button class="bg-transparent ml-2" @click="openMenu" v-if="!showMenu">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />        
+        </svg>
+      </button>
+      
+      <!-- Input para búsqueda -->
+      <input type="text" class="bg-transparent text-white border-b border-blue-500 rounded-xl mx-2 w-56 sm:w-96 focus:border-red-500 outline-none" placeholder="   Buscar en GOING" />
 
+      <!-- Ícono carrito -->
+      <button class="bg-transparent mr-2" @click="navigateToPage">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />        
+        </svg>
+      </button>
+    </div>
 
-    <!-- Ícono carrito -->
-    <button class="bg-transparent mr-2" @click="navigateToPage">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />        
-      </svg>
-    </button>
-
+    <!-- Ícono de ubicación y texto -->
+    <div class="bg-black text-indigo-400 fixed top-14 left-0 right-0 flex items-center justify-start h-10 px-2">
+      <button class="flex items-center">
+        <div class="bg-transparent">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+          </svg>
+        </div>
+          <span class="text-xs ml-1">Código postal</span>
+      </button>
+    </div>
+  </div>
 
     <!-- Contenedor de fondo semitransparente -->
     <div class="bg-gray-600/50 min-h-screen w-full fixed top-0 left-0 right-0 backdrop-blur-sm" @click="closeMenu" v-if="showMenu">
@@ -26,7 +41,7 @@
     <div class="min-h-screen w-60 fixed top-0 left-0 bg-gradient-to-r from-neutral-900 via-teal-900 to-emerald-800">
       <div>
           <!-- Ícono cierre -->
-          <button @click="closeMenu" v-if="showMenu" class="mt-4 ml-2">                               
+          <button @click="closeMenu" v-if="showMenu" class="mt-4 ml-2 text-white">                               
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
@@ -77,7 +92,7 @@
         </div>
       </div>
     </div>
-  </div>
+  
 </template>
 
 
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     <SideBar/>
 
     <!-- Nombre de la empresa y logo -->
-    <div class="flex items-center justify-center p-6 mt-14 rounded-xl bg-gradient-to-r from-neutral-900 via-teal-900 to-purple-950 to-violet-800">
+    <div class="flex items-center justify-center p-6 mt-20 rounded-xl bg-gradient-to-r from-neutral-900 via-teal-900 to-purple-950 to-violet-800">
       <img src="@/assets/LOGODOS.png" alt="Logo empresa" class="h-32 w-auto mr-4">
       <h1 class="text-white">
         <span style="font-size: 48px;" class="block font-bold">GOING</span>
@@ -51,7 +51,7 @@
       <div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-4">  
         <!-- Productos -->        
         <div v-for="(product, index) in products" :key="index" class="shadow-md w-auto h-auto">          
-          <button>                                 
+          <button @click="Onproduct">                                 
             <div class="flex flex-col items-center justify-center bg-white w-auto h-48 rounded-lg">
                 <img :src="product.img" alt="Img. Del producto" class="h-32 w-auto">
             </div>
@@ -61,8 +61,6 @@
                     <p class="text-base text-rose-200 text-left">{{ product.price }}</p>  
                 </div>
             </div>
-
-
           </button>
         </div>          
       </div>
@@ -71,60 +69,66 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      //Productos destacados
-      //Importante * para la descripción solamente se permiten 79 caracteres como maximo.
-      products: [
-        { description: 'Galletas goting en forma de estrella', price: '$10.00', img:'/_nuxt/assets/PruebaUno.png'},
-        { description: 'Anillo brillante con 950 de plata de la marca GOING', price: '$20.00', img:'/_nuxt/assets/anillo.png'},
-        { description: 'Bebida Going de 500ml _ energetizante', price: '$30.00', img:'/_nuxt/assets/drink.png'},
-        { description: 'Libreta con pasta de cuero de caballo', price: '$40.00', img:'/_nuxt/assets/libreta.png'},
-        { description: 'Mochila escolar femenina de 10 a 15 años', price: '$50.00', img:'_nuxt/assets/mochila.png'},
-        { description: 'Tenis rojos GOING talla 27 cm con suela de hule', price: '$60.00', img:'/_nuxt/assets/tenis.png'},
-        { description: 'Collar con dije en forma de corazón GOING', price: '$70.00', img:'/_nuxt/assets/collar.png'},
-        { description: 'Logo en formato png de la empresa GOING', price: '$80.00', img:'/_nuxt/assets/LOGODOS.png'},
-        { description: 'Hamburguesa cheveRe sencilla de una sola pieza de carne', price: '$45.00', img:'/_nuxt/assets/hamburguesa.png'},
-        { description: '1 kilo de tortillas', price: '$12.00', img:'/_nuxt/assets/tortillas.png'},
-        { description: 'Tarro de michelada con o sin clamato y cerveza de su preferencia', price: '$40.00', img:'/_nuxt/assets/michelada.png'},
-        { description: 'Cubo rubik anti-estres de 3x3 de buena calidad', price: '$60.00', img:'/_nuxt/assets/cuborubik.png'},
-        { description: 'Agendar servicio de fletes y mudanzas', price: 'Cotizar', img:'/_nuxt/assets/fletes.png'},
-        { description: 'Carretilla truper para la construcción', price: '$430.00', img:'/_nuxt/assets/carretilla.png'},
-        { description: 'Curso de matemáticas para jovenes de 12 a 18 años de edad', price: 'Cotizar', img:'/_nuxt/assets/curso.png'},
-        { description: 'Agendar cita para la barbería la PELONA', price: 'Cotizar', img:'/_nuxt/assets/servicioBarber.png'},
-        { description: 'Oferta de trabajo para mesera en el restaurante QueCheve-Re', price: 'Ver oferta', img:'/_nuxt/assets/Solicitud.png'}
+  export default {
+    data() {
+      return {
+        //Productos destacados
+        //Importante * para la descripción solamente se permiten 79 caracteres como maximo.
+        products: [
+          { description: 'Galletas goting en forma de estrella', price: '$10.00', img:'/_nuxt/assets/PruebaUno.png'},
+          { description: 'Anillo brillante con 950 de plata de la marca GOING', price: '$20.00', img:'/_nuxt/assets/anillo.png'},
+          { description: 'Bebida Going de 500ml _ energetizante', price: '$30.00', img:'/_nuxt/assets/drink.png'},
+          { description: 'Libreta con pasta de cuero de caballo', price: '$40.00', img:'/_nuxt/assets/libreta.png'},
+          { description: 'Mochila escolar femenina de 10 a 15 años', price: '$50.00', img:'_nuxt/assets/mochila.png'},
+          { description: 'Tenis rojos GOING talla 27 cm con suela de hule', price: '$60.00', img:'/_nuxt/assets/tenis.png'},
+          { description: 'Collar con dije en forma de corazón GOING', price: '$70.00', img:'/_nuxt/assets/collar.png'},
+          { description: 'Logo en formato png de la empresa GOING', price: '$80.00', img:'/_nuxt/assets/LOGODOS.png'},
+          { description: 'Hamburguesa cheveRe sencilla de una sola pieza de carne', price: '$45.00', img:'/_nuxt/assets/hamburguesa.png'},
+          { description: '1 kilo de tortillas', price: '$12.00', img:'/_nuxt/assets/tortillas.png'},
+          { description: 'Tarro de michelada con o sin clamato y cerveza de su preferencia', price: '$40.00', img:'/_nuxt/assets/michelada.png'},
+          { description: 'Cubo rubik anti-estres de 3x3 de buena calidad', price: '$60.00', img:'/_nuxt/assets/cuborubik.png'},
+          { description: 'Agendar servicio de fletes y mudanzas', price: 'Cotizar', img:'/_nuxt/assets/fletes.png'},
+          { description: 'Carretilla truper para la construcción', price: '$430.00', img:'/_nuxt/assets/carretilla.png'},
+          { description: 'Curso de matemáticas para jovenes de 12 a 18 años de edad', price: 'Cotizar', img:'/_nuxt/assets/curso.png'},
+          { description: 'Agendar cita para la barbería la PELONA', price: 'Cotizar', img:'/_nuxt/assets/servicioBarber.png'},
+          { description: 'Oferta de trabajo para mesera en el restaurante QueCheve-Re', price: 'Ver oferta', img:'/_nuxt/assets/Solicitud.png'}
 
-        // Puedes agregar más productos aquí
-      ],
-      categories: [
-        { name: 'Puntos', imgen: '/_nuxt/assets/Categorias/puntos.png'},
-        { name: 'Ofertas', imgen: '/_nuxt/assets/Categorias/ofertas.png'},
-        { name: 'Súper', imgen: '/_nuxt/assets/Categorias/super.png'},
-        { name: 'Moda', imgen: '/_nuxt/assets/Categorias/moda.jpeg'},
-        { name: 'Papeleria', imgen: '/_nuxt/assets/Categorias/papeleria.jpeg'},
-        { name: 'Artículos', imgen: '/_nuxt/assets/Categorias/articulos.jpeg'},
-        { name: 'Material', imgen: '/_nuxt/assets/Categorias/materiales.jpeg'},
-        { name: 'Servicios', imgen: '/_nuxt/assets/Categorias/servicios.jpeg'}                  
-      ]
-    };
-  },
-  mounted() {
-    let carouselItems = document.querySelectorAll('.carousel-item');
+          // Puedes agregar más productos aquí
+        ],
+        categories: [
+          { name: 'Puntos', imgen: '/_nuxt/assets/Categorias/puntos.png'},
+          { name: 'Ofertas', imgen: '/_nuxt/assets/Categorias/ofertas.png'},
+          { name: 'Súper', imgen: '/_nuxt/assets/Categorias/super.png'},
+          { name: 'Moda', imgen: '/_nuxt/assets/Categorias/moda.jpeg'},
+          { name: 'Papeleria', imgen: '/_nuxt/assets/Categorias/papeleria.jpeg'},
+          { name: 'Artículos', imgen: '/_nuxt/assets/Categorias/articulos.jpeg'},
+          { name: 'Material', imgen: '/_nuxt/assets/Categorias/materiales.jpeg'},
+          { name: 'Servicios', imgen: '/_nuxt/assets/Categorias/servicios.jpeg'}                  
+        ]
+      };    
+    },
+    mounted() {
+      let carouselItems = document.querySelectorAll('.carousel-item');
 
-    function toggleCarousel() {
-      let activeItemIndex = 0;
+      function toggleCarousel() {
+        let activeItemIndex = 0;
 
-      setInterval(function() {
-        carouselItems[activeItemIndex].classList.remove('carousel-item');
-        activeItemIndex = (activeItemIndex + 1) % carouselItems.length;
-        carouselItems[activeItemIndex].classList.add('carousel-item');
-      }, 5000); // Cambia este valor para ajustar la velocidad de movimiento
-    }
+        setInterval(function() {
+          carouselItems[activeItemIndex].classList.remove('carousel-item');
+          activeItemIndex = (activeItemIndex + 1) % carouselItems.length;
+          carouselItems[activeItemIndex].classList.add('carousel-item');
+        }, 5000); // Cambia este valor para ajustar la velocidad de movimiento
+      }
 
-    toggleCarousel();
-  }
-};
+      toggleCarousel();
+    },
+    methods: {
+      Onproduct() {
+        // Navegar a la otra página utilizando Vue Router
+        this.$router.push('/products/product');
+      },
+    },
+  };
 </script>
 
 <style>
