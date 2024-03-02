6d7b37753ae03511e7484e013b95f3ad86e0081e
e-duardo-lucas <el035627@gmail.com>
2024-03-01 21:17:57 -0600

    Updates

--- assets/Favoritos.hMj7mGmg.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as e,o as t}from"./entry.8cLIvlG5.js";const a={};function c(r,s){return t(),e("div",null," Hola, aquí estan tus favoritos ")}const _=o(a,[["render",c]]);export{_ as default};
--- assets/Perfil.PrIYryN2.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./entry.8cLIvlG5.js";const r={};function t(n,a){return o(),c("div",null," Hola, Soy tu perfil ")}const s=e(r,[["render",t]]);export{s as default};
