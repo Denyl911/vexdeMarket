b6cca851d0f9c239f7a37067ec5ce2236d0b35de
e-duardo-lucas <el035627@gmail.com>
2024-03-01 20:02:24 -0600

    Updates

---
+++ assets/Perfil.rmOf5VG1.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./entry.ibtDzJY1.js";const r={};function t(n,a){return o(),c("div",null," Hola, Soy tu perfil ")}const s=e(r,[["render",t]]);export{s as default};
---
+++ assets/SoyVendedor.WG9u-aLp.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as c}from"./entry.ibtDzJY1.js";const n={};function r(t,a){return c(),o("div",null," Hola, Soy Vendedor ")}const _=e(n,[["render",r]]);export{_ as default};
